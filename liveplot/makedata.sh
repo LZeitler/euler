5a68cf8aef4a7b4593077d8e62db5f15b4931ad6
Leo Zeitler <zeitlerl@lo-login-01.leonhard.ethz.ch>
2020-02-21 17:42:10 +0100

    Works better now

--- liveplot/makedata.sh
+++ liveplot/makedata.sh
@@ -6,12 +6,19 @@ JOB=$1
 
 ITER=0
 writedata() {
-    if [ -n $(bbjobs -l $JOB | grep -m 1 -e "Resident" | grep -Eo '[0-9]+[.][0-9]') ]; then
-	echo "Job "$JOB" is running. Collecting data..."
+    if $(bbjobs -l $JOB | grep -q "RUNNING"); then 
+	echo "Job "$JOB" is running."
     else
-	echo "Job not running. Starting to write when job starts"
+	echo "Job "$JOB" not running. Starting to write when job starts"
+	while $(bbjobs -l $JOB | grep -q "PENDING"); do
+	    sleep 10
+	done
     fi
-    while [ -n $(bbjobs -l $JOB | grep -m 1 -e "Resident" | grep -Eo '[0-9]+[.][0-9]') ]; do 
+    
+    echo "Collecting data now ..." 
+    sleep 5
+
+    while $(bbjobs -l $JOB | grep -q "RUNNING"); do 
 	NUM=$(bbjobs -l $JOB | grep -m 1 -e "Resident" | grep -Eo '[0-9]+[.][0-9]')
 	echo -e ${ITER}'\t'${NUM} >> plot.dat
 	sleep 10
@@ -19,4 +26,7 @@ writedata() {
     done
 }
 
-writedata
+writedata &
+sleep 11
+echo "Plotting ..."
+gnuplot liveplot.gnu
